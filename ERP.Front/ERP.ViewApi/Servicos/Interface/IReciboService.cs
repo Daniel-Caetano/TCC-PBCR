ddfa3c3593f905c0f8e92a9123b339c4c3c965a7
Daniel Caetano de Souza Ferreira <danielcaetano.sf@gmail.com>
2022-01-20 14:52:04 -0300

    Adicionado ViewApi

---
+++ ERP.Front/ERP.ViewApi/Servicos/Interface/IReciboService.cs
@@ -0,0 +1,18 @@
+﻿using ERP.ViewApi.Negocio;
+using Refit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ERP.ViewApi.Servicos.Interface
+{
+
+    public interface IReciboService
+    {
+        [Get("/Recibo/Imprimir/json")]
+        Task<IList<ReciboResponse>> GetAsync();
+    }
+
+}
