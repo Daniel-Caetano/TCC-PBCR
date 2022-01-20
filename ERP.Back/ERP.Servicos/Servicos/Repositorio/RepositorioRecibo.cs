e9ea7c09dfc39a9ea6e0bbee401977c98733bc62
Guilherme <guilherme.silva@aliare.co>
2022-01-20 16:12:46 -0300

    Atualizando Classe Pessoas

--- ERP.Back/ERP.Servicos/Servicos/Repositorio/RepositorioRecibo.cs
+++ ERP.Back/ERP.Servicos/Servicos/Repositorio/RepositorioRecibo.cs
@@ -20,7 +20,7 @@ namespace ERP.Servicos
         {
             var pessoas = new List<Pessoa>();
             var sql = new StringBuilder()
-                .AppendLine("SELECT * FROM PESSOA PE WHERE PE.[CPF_PESSOA] = @cpf");
+                .AppendLine("SELECT * FROM PESSOAS PE WHERE PE.[PESS_CPF] = @cpf");
 
             using (var conn = new SqlConnection(_stringConexao))
             {
@@ -32,11 +32,10 @@ namespace ERP.Servicos
                 while (reader.Read())
                 {
                     var pessoa = new Pessoa();
-                    pessoa.Nome = reader.GetString(reader.GetOrdinal("NOME_PESSOA"));
-                    pessoa.ID = reader.GetInt32(reader.GetOrdinal("ID_PESSOA"));
-                    pessoa.CPF = reader.GetString(reader.GetOrdinal("CPF_PESSOA"));
-                    pessoa.Endereco = reader.GetString(reader.GetOrdinal("ENDERECO_PESSOA"));
-                    pessoa.Telefone = reader.GetString(reader.GetOrdinal("TELEFONE_PESSOA"));
+                    pessoa.ID = reader.GetInt32(reader.GetOrdinal("PESS_ID_PK"));
+                    pessoa.Nome = reader.GetString(reader.GetOrdinal("PESS_NOM"));
+                    pessoa.CPF = reader.GetString(reader.GetOrdinal("PESS_CPF"));
+                    pessoa.Endereco = reader.GetInt32(reader.GetOrdinal("PESS_ENDE_ID_FK"));
 
                     pessoas.Add(pessoa);
                 }
