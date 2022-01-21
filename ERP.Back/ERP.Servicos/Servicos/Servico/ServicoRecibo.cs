2f6ff0188a7ed57bd8b758bbc1d2f563eaa02ad6
Daniel Caetano de Souza Ferreira <danielcaetano.sf@gmail.com>
2022-01-21 08:29:10 -0300

    Refatorano projeto Servico

--- ERP.Back/ERP.Servicos/Servicos/Servico/ServicoRecibo.cs
+++ ERP.Back/ERP.Servicos/Servicos/Servico/ServicoRecibo.cs
@@ -14,24 +14,6 @@ namespace ERP.Servico.Servicos.Servico
         {
             _stringConexao = stringConexao;
         }
-        public List<Pessoa> ConsultaDados(string cpf)
-        {
-            try
-            {
-                _ = cpf.Length != 11;
-                new CPFValidator().AssertValid(cpf);
-            }
-
-            catch (Exception ex)
-            {
-                Debug.WriteLine("CPF invalido!" + ex.Message);
-            }
-
-            var repo = new RepositorioRecibo(_stringConexao);
-            var listaPessoas = repo.ConsultaDados(cpf);
-            return listaPessoas;
-        }
-
         public List<Recibo> GeraRecibo()
         {
             var repo = new RepositorioRecibo(_stringConexao);
