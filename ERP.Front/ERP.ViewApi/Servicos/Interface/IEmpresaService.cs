56d6c086c2dd97dd8cf4de188a4fdd785082eeda
Daniel Caetano de Souza Ferreira <91154445+Daniel-Caetano@users.noreply.github.com>
2022-01-25 00:57:57 -0300

    Consumindo Api para buscar empresa pelo cnpj

---
+++ ERP.Front/ERP.ViewApi/Servicos/Interface/IEmpresaService.cs
@@ -0,0 +1,17 @@
+﻿using ERP.ViewApi.Negocio;
+using Refit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ERP.ViewApi.Servicos.Interface
+{
+	public interface IEmpresaService
+	{
+
+		[Get("/Empresa/CNPJ/{cnpj}/json")]
+		Task<IList<EmpresaResponse>> GetAsync(string cnpj);
+	}
+}
