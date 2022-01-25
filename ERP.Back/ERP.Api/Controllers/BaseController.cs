d337cfa90c7fd5d5d202873b35630dde75a993c1
Guilherme <guilherme.silva@aliare.co>
2022-01-25 11:38:57 -0300

    Implementando Buscas do Recibo

--- ERP.Back/ERP.Api/Controllers/BaseController.cs
+++ ERP.Back/ERP.Api/Controllers/BaseController.cs
@@ -2,9 +2,8 @@
 
 namespace ERP.Api.Controllers
 {
-    public class BaseController : ControllerBase
+    public class BaseController : ControllerBase // Realiza a Conexão com o Banco de Dados 
     {
-        // protected const string _connectionString = "Server=(localdb)\\mssqllocaldb;Database=ERP;Trusted_Connection=True;User Id = sa; Password=Daniel123@";
         protected const string _connectionString = "Server=localhost,1401;Database=ERP;User Id = sa; Password=Tccpbcr123@";
 
     }
