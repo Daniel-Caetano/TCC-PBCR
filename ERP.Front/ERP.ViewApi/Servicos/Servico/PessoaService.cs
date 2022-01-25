002f106f56776bc897c3de4bf7abf8e02fb95563
Daniel Caetano de Souza Ferreira <91154445+Daniel-Caetano@users.noreply.github.com>
2022-01-25 10:52:10 -0300

    Implementando CRUD de empresa e BUSCA por CPF de pessoa

---
+++ ERP.Front/ERP.ViewApi/Servicos/Servico/PessoaService.cs
@@ -0,0 +1,46 @@
+﻿using ERP.ViewApi.Negocio;
+using ERP.ViewApi.Servicos.Interface;
+using Refit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ERP.ViewApi.Servicos.Servico
+{
+    public class PessoaService
+    {
+        private readonly IPessoaService _customerService;
+        private readonly string _apiUrl;
+
+        public PessoaService()
+        {
+            _apiUrl = "http://localhost:50663";
+            _customerService = RestService.For<IPessoaService>(_apiUrl);
+        }
+
+        public async Task<IList<PessoaResponse>> GetAsync(string cpf)
+        {
+            return await _customerService.GetAsync(cpf);
+        }
+        public async Task<IList<PessoaResponse>> GetAsyncNome(string nome)
+        {
+            return await _customerService.GetAsync(nome);
+        }
+        public async Task<IList<PessoaResponse>> InsertAsync(string Nome, string CPF, string NumeroEndereco, string Complemento, string CEP, string Logradouro, string Bairro, string Localidade, string UF)
+        {
+            return await _customerService.InsertAsync(Nome, CPF, NumeroEndereco, Complemento, CEP, Logradouro, Bairro, Localidade, UF);
+        }
+        public async Task<IList<PessoaResponse>> UpdateAsync(string CpfAtual, string Nome, string CPF,
+            string NumeroEndereco, string Complemento, string CEP
+            , string Logradouro, string Bairro, string Localidade, string UF)
+        {
+            return await _customerService.UpdateAsync(CpfAtual, Nome, CPF, NumeroEndereco, Complemento, CEP, Logradouro, Bairro, Localidade, UF);
+        }
+        public async Task DeleteAsync(string cpf)
+        {
+            await _customerService.DeleteAsync(cpf);
+        }
+    }
+}
